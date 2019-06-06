9d1c512525d360cb75c98bd009c2629674707746
lavajiit <lavajiit@yandex.com>
2019-06-06 16:20:47 +0530

    removed extra file

--- practice.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-import warnings
-warnings.filterwarnings("ignore")
-np.random.seed(37)
-df = pd.read_csv("/home/f/Data/Titanic_Data/train.csv")
-print(df.columns)
